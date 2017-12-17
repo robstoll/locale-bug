314a04b61b2f5079232802125558d50da9cf21af
Robert Stoll <rstoll@tutteli.ch>
2017-12-18 00:04:15 +0100

    test case for Chinese

--- src/test/java/LocaleTest.java
+++ src/test/java/LocaleTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class LocaleTest
 {
     @Test
-    public void test() {
+    public void testNorwegian() {
         ResourceBundle rb = ResourceBundle.getBundle("RBundle", new Locale("no", "NO", "NY"));
         assertEquals("Hello in nn_NO", rb.getString("hello"));
         assertEquals("say in nn_NO", rb.getString("say"));
@@ -18,5 +18,19 @@ public class LocaleTest
         assertEquals("Hello in nn_NO", rb.getString("hello"));
         assertEquals("say in nn_NO", rb.getString("say"));
         assertEquals("Hello in nn_NO", rb.getString("hello"));
+    }
+
+    @Test
+    public void testChinese() {
+        ResourceBundle rb = ResourceBundle.getBundle("RBundle", new Locale("zh", "TW"));
+        assertEquals("Hello in zh_Hant_TW", rb.getString("hello"));
+        assertEquals("say in zh_Hant_TW", rb.getString("say"));
+        assertEquals("Hello in zh_Hant_TW", rb.getString("hello"));
+        // so far so good, because caching did not apply, we loaded the bundle only once
+        // we won't get the same result if we load the bundle again (due to cashing)
+        rb = ResourceBundle.getBundle("RBundle", new Locale("zh", "TW"));
+        assertEquals("Hello in zh_Hant_TW", rb.getString("hello"));
+        assertEquals("say in zh_Hant_TW", rb.getString("say"));
+        assertEquals("Hello in zh_Hant_TW", rb.getString("hello"));
     }
 }
